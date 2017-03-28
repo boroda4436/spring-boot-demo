524a35aa4bb36cf12d5c52598cc0d7b39d7b0b89
Bohdan <b.chupika@globalmoney.ua>
2017-03-28 15:41:11 +0300

    add html, js and css files

--- src/main/java/ua/com/jon/web/HomeController.java
+++ src/main/java/ua/com/jon/web/HomeController.java
@@ -1,19 +1,23 @@
 package ua.com.jon.web;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Bohdan on 27.03.2017
  */
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class HomeController {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "index";
     }
 
 }
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by b.chupika on 28.03.2017.
+ */
+$(document).ready(function () {
+
+   $('#myJsDiv').text("I'm jQuery added text")
+});
